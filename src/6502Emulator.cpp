dcf8da735235737a4955fd026aed5b15848cc2fa
David O'Mahony <domahony@compusult.net>
2015-01-30 19:07:55 -0330

    Adding eclipse project artifacts to github.

---
+++ src/6502Emulator.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : 6502Emulator.cpp
+ Author      : David O'Mahony
+ Version     :
+ Copyright   : MIT
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
